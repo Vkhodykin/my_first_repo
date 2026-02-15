b7111ead5b61f4ae0b930b7a0b48f5c67cd86543
Vkhodykin <victor.khodykin@gmail.com>
2026-02-15 20:33:37 +0300

    Подключил модуль display в core и добавил информационное сообщение

--- src/core.py
+++ src/core.py
@@ -1,14 +1,17 @@
+from display import show_info_message, show_error_message
 
 
-
-def start()
+def start():
     """
     Запуск
     :return: 
     """
-    pass
 
-def main_loop()
+    show_info_message("Для начала работы выберите что вы хотите сделать:\n 1 - info\n 2 - add\n 3 - show\n 4 - del\n"
+                      "5 - edit\n 6 - find\n 7 - type\n 8 - cat\n 9 - period\n 10 - stats\n 11 - exit")
+
+
+def main_loop():
     """
     Работа
     :return: 
