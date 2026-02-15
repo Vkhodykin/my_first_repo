9ea4badb892abe9c577ec57aef7a08dd82fe1c51
Vkhodykin <victor.khodykin@gmail.com>
2026-02-15 17:09:29 +0300

    Добавил businesslogic_lower.py и перенес в него вспомогательные функции

---
+++ src/businesslogic_lower.py
@@ -0,0 +1,15 @@
+
+def validate_type_transaction(type_transaction):
+    pass
+
+def validate_amount(amount):
+    pass
+
+def validate_category(category):
+    pass
+
+def validate_description(description):
+    pass
+
+def write_journal_entry(type_transaction, amount, category, description):
+    pass
