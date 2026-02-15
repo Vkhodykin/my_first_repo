40a9cb832f7fda24199f2c496a154cef8c6068a4
Vkhodykin <victor.khodykin@gmail.com>
2026-02-15 22:35:49 +0300

    Исправил значение функции add_journal_entry в businesslogic_upper на булево

--- src/businesslogic_upper.py
+++ src/businesslogic_upper.py
@@ -1,25 +1,28 @@
-from businesslogic_lower import *
+from src import businesslogic_lower as bll
 
-def add_journal_entry(type_transaction: str, amount: float, category: str, description: str) -> None:
 
-    if not validate_type_transaction(type_transaction):
-        raise ValueError("Invalid transaction type")
+def try_add_journal_entry(type_transaction: str, amount: float, category: str, description: str) -> bool:
 
-    if not validate_amount(amount):
-        raise ValueError("Invalid amount")
+    if not bll.validate_type_transaction(type_transaction):
+        return False
 
-    if not validate_category(category):
-        raise ValueError("Invalid category")
+    if not bll.validate_amount(amount):
+        return False
 
-    if not validate_description(description):
-        raise ValueError("Invalid description")
+    if not bll.validate_category(category):
+        return False
 
-    write_journal_entry(type_transaction, amount, category, description)
+    if not bll.validate_description(description):
+        return False
 
-    pass
+    bll.write_journal_entry(type_transaction, amount, category, description)
+
+    return True
+
 
 def show_entries():
     pass
+
 
 def find_entries_by_type(type_transaction):
     pass
