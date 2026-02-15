bde1c54369839bb2a1bcfde140b18d73495b0b95
Vkhodykin <victor.khodykin@gmail.com>
2026-02-15 17:04:02 +0300

    Добавил businesslogic_upper.py и перенес в него функции работы с записями журнала

--- main.py
+++ main.py
@@ -39,24 +39,3 @@ def write_journal_entry(type_transaction, amount, category, description):
     pass
 
 
-def add_journal_entry(type_transaction: str, amount: float, category: str, description: str) -> None:
-
-    if not validate_type_transaction(type_transaction):
-        raise ValueError("Invalid transaction type")
-
-    if not validate_amount(amount):
-        raise ValueError("Invalid amount")
-
-    if not validate_category(category):
-        raise ValueError("Invalid category")
-
-    if not validate_description(description):
-        raise ValueError("Invalid description")
-
-    write_journal_entry(type_transaction, amount, category, description)
-
-    pass
-
-def show_entries():
-    pass
-
---
+++ src/businesslogic_upper.py
@@ -0,0 +1,25 @@
+
+def add_journal_entry(type_transaction: str, amount: float, category: str, description: str) -> None:
+
+    if not validate_type_transaction(type_transaction):
+        raise ValueError("Invalid transaction type")
+
+    if not validate_amount(amount):
+        raise ValueError("Invalid amount")
+
+    if not validate_category(category):
+        raise ValueError("Invalid category")
+
+    if not validate_description(description):
+        raise ValueError("Invalid description")
+
+    write_journal_entry(type_transaction, amount, category, description)
+
+    pass
+
+def show_entries():
+    pass
+
+def find_entries_by_type(type_transaction):
+    pass
+
