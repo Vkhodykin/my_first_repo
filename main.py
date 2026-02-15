c79a4b03816b287714bfc67c8e65e045d5f6724b
Vkhodykin <victor.khodykin@gmail.com>
2026-02-15 16:29:23 +0300

    Добавил проверку функции add_journal_entry и проверочные функции в main.py

--- main.py
+++ main.py
@@ -23,7 +23,38 @@ def stop():
     pass
 
 
+def validate_type_transaction(type_transaction):
+    pass
+
+def validate_amount(amount):
+    pass
+
+def validate_category(category):
+    pass
+
+def validate_description(description):
+    pass
+
+def write_journal_entry(type_transaction, amount, category, description):
+    pass
+
+
 def add_journal_entry(type_transaction: str, amount: float, category: str, description: str) -> None:
+
+    if not validate_type_transaction(type_transaction):
+        raise ValueError("Invalid transaction type")
+
+    if not validate_amount(amount):
+        raise ValueError("Invalid amount")
+
+    if not validate_category(category):
+        raise ValueError("Invalid category")
+
+    if not validate_description(description):
+        raise ValueError("Invalid description")
+
+    write_journal_entry(type_transaction, amount, category, description)
+
     pass
 
 def show_entries():
