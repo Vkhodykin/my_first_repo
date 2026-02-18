46046f20b392fa31886e9221fda1c61271344288
Vkhodykin <victor.khodykin@gmail.com>
2026-02-18 17:33:33 +0300

    Разделил запись в журнал в зависимости от типа операции в businesslogic_upper

--- src/businesslogic_upper.py
+++ src/businesslogic_upper.py
@@ -1,8 +1,8 @@
 from src import businesslogic_lower as bll
 
 
-def try_add_journal_entry(create_id_generator: str, type_transaction: str, amount: float, category_income: str | None,
-                          category_expense: str | None, description_income: str | None, description_expense: str | None) -> bool:
+def try_add_journal_entry_income(create_id_generator: str, type_transaction: str, amount: float, category_income: str,
+                          description_income: str) -> bool:
 
     if not bll.create_id_generator():
         return False
@@ -16,17 +16,33 @@ def try_add_journal_entry(create_id_generator: str, type_transaction: str, amoun
     if not bll.validate_category_income(category_income):
         return False
 
-    if not bll.validate_category_expense(category_expense):
+    if not bll.validate_description_income(description_income):
         return False
 
-    if not bll.validate_description_income(description_income):
+    bll.write_journal_entry_income(create_id_generator, type_transaction, amount, category_income, description_income)
+
+    return True
+
+
+def try_add_journal_entry_expense(create_id_generator: str, type_transaction: str, amount: float, category_expense: str,
+                                  description_expense: str) -> bool:
+
+    if not bll.create_id_generator():
+        return False
+
+    if not bll.validate_type_transaction(type_transaction):
+        return False
+
+    if not bll.validate_amount(amount):
+        return False
+
+    if not bll.validate_category_expense(category_expense):
         return False
 
     if not bll.validate_description_expense(description_expense):
         return False
 
-    bll.write_journal_entry(create_id_generator, type_transaction, amount, category_income, category_expense,
-                            description_income, description_expense)
+    bll.write_journal_entry_expense(create_id_generator, type_transaction, amount, category_expense, description_expense)
 
     return True
 
