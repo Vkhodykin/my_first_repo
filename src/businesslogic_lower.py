d422a8b77aa8b1b41de200e1bcb32e7a050953df
Vkhodykin <victor.khodykin@gmail.com>
2026-02-18 13:09:48 +0300

    Добавил генератор id в businesslogic_lower.py

--- src/businesslogic_lower.py
+++ src/businesslogic_lower.py
@@ -1,3 +1,24 @@
+import itertools
+from typing import Callable
+
+
+def create_id_generator(start=1, prefix='') -> Callable[[], str]:
+    """
+    Генератор последовательных ID
+    """
+
+    counter = start
+
+    def generate() -> str:
+
+        nonlocal counter
+        current = counter
+        counter += 1
+
+        return f"{prefix}{current}" if prefix else current
+
+    return generate
+
 
 def validate_type_transaction(type_transaction):
     pass
