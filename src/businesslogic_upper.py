acb1f69e0b5ba461aa61b70857e92c923dde0bd8
Vkhodykin <victor.khodykin@gmail.com>
2026-02-18 16:31:50 +0300

    Разделил категории и описания в зависимости от типа операции в businesslogic_upper

--- src/businesslogic_upper.py
+++ src/businesslogic_upper.py
@@ -1,7 +1,11 @@
 from src import businesslogic_lower as bll
 
 
-def try_add_journal_entry(type_transaction: str, amount: float, category: str, description: str) -> bool:
+def try_add_journal_entry(create_id_generator: str, type_transaction: str, amount: float, category_income: str | None,
+                          category_expense: str | None, description_income: str | None, description_expense: str | None) -> bool:
+
+    if not bll.create_id_generator():
+        return False
 
     if not bll.validate_type_transaction(type_transaction):
         return False
@@ -9,13 +13,20 @@ def try_add_journal_entry(type_transaction: str, amount: float, category: str, d
     if not bll.validate_amount(amount):
         return False
 
-    if not bll.validate_category(category):
+    if not bll.validate_category_income(category_income):
         return False
 
-    if not bll.validate_description(description):
+    if not bll.validate_category_expense(category_expense):
         return False
 
-    bll.write_journal_entry(type_transaction, amount, category, description)
+    if not bll.validate_description_income(description_income):
+        return False
+
+    if not bll.validate_description_expense(description_expense):
+        return False
+
+    bll.write_journal_entry(create_id_generator, type_transaction, amount, category_income, category_expense,
+                            description_income, description_expense)
 
     return True
 
