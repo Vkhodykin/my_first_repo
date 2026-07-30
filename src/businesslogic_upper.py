b907cfd0664cb8bfc05742b6aca444f72912847b
Vkhodykin <victor.khodykin@gmail.com>
2026-07-30 23:45:27 +0300

    Исправил функцию попытки записи расходной операции в businesslogic_upper.py

--- src/businesslogic_upper.py
+++ src/businesslogic_upper.py
@@ -63,30 +63,33 @@ def try_add_journal_entry_income(gen_total,
     return True
 
 
-def try_add_journal_entry_expense(create_id_generator: int, type_transaction: str, amount: float, category_expense: str,
-                                  description_expense: str, get_current_datetime: str) -> bool:
+def try_add_journal_entry_expense(gen_total, 
+                                  gen_expense, 
+                                  input_type_transaction: str,
+                                  input_amount: str, 
+                                  category_input: str,
+                                  description_type: str, 
+                                  date: str) -> bool:
 
-    if not bll.create_id_generator():
+    if not bll.validate_type_transaction(input_type_transaction):
         return False
 
-    if not bll.validate_type_transaction(type_transaction):
+    if not bll.validate_amount(input_amount):
         return False
 
-    if not bll.validate_amount(amount):
+    if not bll.validate_category(category_input, 'expense'):
         return False
 
-    if not bll.validate_category_expense(category_expense):
+    if not bll.validate_description_expense(description_type):
         return False
 
-    if not bll.validate_description_expense(description_expense):
-        return False
-
-    if not bll.get_current_datetime():
-        return False
-
-    bll.write_journal_entry_expense(create_id_generator, type_transaction, amount, category_expense, description_expense,
-                                    get_current_datetime)
-
+    bll.write_journal_entry_expense(gen_total, 
+                                    gen_expense,
+                                    input_type_transaction,
+                                    input_amount, 
+                                    category_input, 
+                                    description_type,
+                                    date)
     return True
 
 
