e230cd10cf4823190054c893935c9374e05e73b1
Vkhodykin <victor.khodykin@gmail.com>
2026-08-01 23:33:28 +0300

    Добавил ввод данных и вызов функции попытки записи для расходных операций в core.py

--- src/core.py
+++ src/core.py
@@ -111,23 +111,71 @@ def main_loop():
                         display.show_error_message("Ошибка при записи операции")
                         display.show_info_message("Операция не записана")
 
+
+                elif input_type_transaction == "expense":
+
+                    expense_amount = True
+                    while expense_amount:
+
+                        display.show_info_message("Введите сумму расхода:")
+                        input_amount = input(">> ")
+
+                        if blu.validate_amount(input_amount):
+                            break
+
+
+                    expense_category = True
+                    while expense_category:
+
+                        display.show_info_message("Введите категорию расходов (Mandatory/Optional/Saving):")
+                        category_input = input(">> ")
+
+                        if blu.validate_category(category_input, 'expense'):
+                            break
+
+
+                    expense_description = True
+                    while expense_description:
+
+                        display.show_info_message("Введите описание расходов (не более 255 символов):")
+                        description_type = input(">> ")
+
+                        if blu.validate_description(description_type):
+                            break
+
+
+                    write_operation = blu.try_add_journal_entry_expense(gen_total,
+                                                                        gen_expense,
+                                                                        input_type_transaction,
+                                                                        input_amount,
+                                                                        category_input,
+                                                                        description_type,
+                                                                        date)
+
+                    if write_operation:
+                        display.show_notification_message("Операция записана успешно")
+
+                        display.show_confirmation_message("Хотите продолжить запись данных? (y/n)")
+                        confirmed = su.confirm(">> ")
+
+                        if confirmed:
+                            # продолжить
+                            continue
+                        else:
+                            # отменить
+                            break
+
+
+                    else:
+                        display.show_error_message("Ошибка при записи операции")
+                        display.show_info_message("Операция не записана")
+
+
                 else:
                     display.show_info_message("Введен неверный тип операции")
                     continue
 
-
-            elif type_transaction == "expense":
-
-                category_expense = input("Введите категорию расходов (Mandatory/Optional/Saving) >> ").strip().lower()
-
-                description_expense = input("Введите описание расходов (не более 255 символов) >> ")
-
-                if blu.try_add_journal_entry_expense(gen_total, type_transaction, amount, category_expense,
-                                                     description_expense, date):
-
-                    display.show_info_message("Операция записана успешно")
-
-
+            
             else:
                 display.show_error_message("Введено некорректное значение. Введите еще раз")
 
